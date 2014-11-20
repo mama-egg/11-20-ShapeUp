14aaf119ee842f7c75fe4693325b916f633b2809
mama-egg <Kinseygoesrawr@gmail.com>
2014-11-20 07:52:40 -0700

    Update Point.java
    Created public class Point

--- src/Point.java
+++ src/Point.java
@@ -1,1 +1,11 @@
 // A Point object represents a pair of (x, y) coordinates.
+public class Point {
+  
+  Point a = new Point(0, 0); //Construct a point at (0, 0)
+  
+  public static void Pointb(int x, int y) {
+    
+    Point b = new Point(x, y); //Construct a point at (x, y)
+  }
+  
+}
