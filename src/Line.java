c85da2347b80c1dbbcd7a9b6a01c4e57a35b5228
mama-egg <Kinseygoesrawr@gmail.com>
2014-11-21 08:17:49 -0700

    Update Line.java

--- src/Line.java
+++ src/Line.java
@@ -3,13 +3,18 @@ public class Line {
   Point start;
   Point end;
   Point p3 = new Point(0, 0);
+  int a;
+  int b;
   
-  public Line(Point p3) {
+  public Line(Point p3, int a, int b) {
     double slopezero = (p3.y - p3.y)/(p3.x - p3.x);
     double plusb = p3.y - (slopezero * p3.x);
+    this.x = this.x + a;
+    this.y = this.y + b;
+    
   }
   
-  public Line(Point start, Point end) {
+  public Line(Point start, Point end, int a, int b) {
     double slope = (end.y - start.y)/(end.x - start.x);
     double b = start.y - (slope * start.x);
     
@@ -17,7 +22,12 @@ public class Line {
     int tempx = end.x - start.x;
     int tempy2 = (end.y - start.y)*(end.y - start.y);
     int tempx2 = (end.x - start.x)*(end.x - start.x);
-    double length = 
+    double length = Math.sqrt(tempy2 + tempx2);
+    
+    this.x = this.x + a;
+    this.y = this.y + b;
     
   }
+  
+  
 }
