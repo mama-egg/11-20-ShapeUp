27288a7b293894141fda52c113c9ea62327d3d1f
mama-egg <Kinseygoesrawr@gmail.com>
2014-12-14 20:04:48 -0700

    Update Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -6,44 +6,53 @@ public class Circle {
   
   //Constructors
   public Circle() {
-    
+    center = 0;
+    radius = 0;
   }
   
-  public Circle(Point startpoint, Point endpoint) {
-    
+  public Circle(Point startPoint, Point endPoint) {
+    center = startPoint;
+    radius = endPoint.x - startPoint.x;
   }
   
-  public Circle(Point center, double radius) {
-    
+  public Circle(Point p1, double d1) {
+    center = p1;
+    radius = d1;
   }
   
   //Accessors
   public double getRadius() {
-    
+    return radius;
   }
   
   public double area() {
-    
+    return π*radius*radius;
   }
   
   public double circumference() {
-    
+    return 2*π*radius;
   }
   
   public double diameter() {
-    
+    return 2*radius;
   }
   
   public String toString() {
-    
+    return "(" + center.x + ", " + center.y + ")" + radius;
   }
   
   //Mutators
-  public void translate() {
+  public void translate(int a, int b) {
+    center.x += a;
+    center.y += b;
+    /*I'm a bit confused about this part-the README says to add 
+    a to the X value of each end point and to add b to the Y 
+    value of each point. 
     
+    Does that mean it affects only the center, or do I just not understand?*/
   }
   
-  public void changeRadius() {
-    
+  public void changeRadius(double d2) {
+    radius += d2;
   }
 }
